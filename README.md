1d5187b481e1173ef3ae690885e7bbb3ea90fe3a
Kevin Bastien <kevinjbastien@gmail.com>
2015-04-22 22:06:11 -0700

    WIP: play drake songs

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # drizzy
-A chrome extension that makes it rain with Drakes face, and replaces all the images on the page with images of Drake.
+A chrome extension that makes it rain with Drakes face, and replaces all the images on the page with random gifs of Drake.
+
+APIs used: Giphy, Soundcloud
--- scripts/drizzy.js
+++ scripts/drizzy.js
@@ -1,15 +1,16 @@
 var giphy = $.get("https://api.giphy.com/v1/gifs/search?q=drake&api_key=dc6zaTOxFJmzC&limit=70");
-giphy.done(function(data) { 
-	var imgCount = document.images.length;
-	console.log("Number of images: " + imgCount);
-	for (var i = 0; i <= imgCount ; i++) {	
-		var rando = Math.floor((Math.random() * 49) + 1);
+giphy.done(
+	function(data) { 
+		var imgCount = document.images.length;
+		console.log("Number of images: " + imgCount);
 		$(document).ready(function(){
-		  	var drizzy = data.data[rando].images.original.url;
-			document.images[i].src = drizzy;
+			for (var i = 0; i < imgCount ; i++) {	
+				var rando = Math.floor((Math.random() * 49) + 1);
+				var drizzy = data.data[rando].images.original.url;
+				document.images[i].src = drizzy;
+			};
 		});
-	};
-	$(document).ready(function(){
-		$(document).snowfall({image :"http://www.lilwaynehq.com/forums/images/smilies/drizzy-face.png", minSize: 15, maxSize:40, flakeCount : 100, maxSpeed : 10});
-	});
-}); 
+		$(document).ready(function(){
+			$(document).snowfall({image :"http://www.lilwaynehq.com/forums/images/smilies/drizzy-face.png", minSize: 15, maxSize:40, flakeCount : 100, maxSpeed : 10});
+		});
+	}); 
