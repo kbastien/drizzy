f6046abeab4ea1092bd03cdd437060411178b680
Kevin Bastien <kevinjbastien@gmail.com>
2015-04-22 20:32:37 -0700

    Icons, and random gif for every image added. WIP: soundcloud api to play music

--- scripts/background.js
+++ scripts/background.js
@@ -2,11 +2,11 @@ var toggle = false;
 chrome.browserAction.onClicked.addListener(function(tab) {
   toggle = !toggle;
   if(toggle){
-    chrome.browserAction.setIcon({path: "icon.png", tabId:tab.id});
+    chrome.browserAction.setIcon({path: "assets/iconOn.png", tabId:tab.id});
     chrome.tabs.executeScript(tab.id, {file:"scripts/drizzy.js"});
   }
   else{
-    chrome.browserAction.setIcon({path: "icon.png", tabId:tab.id});
-    chrome.tabs.executeScript(tab.id, {code:"alert('YOLO')"});
+    chrome.browserAction.setIcon({path: "assets/icon.png", tabId:tab.id});
+    chrome.tabs.executeScript(tab.id, {code:"location.reload()"});
   }
 });
--- scripts/drizzy.js
+++ scripts/drizzy.js
@@ -1,12 +1,14 @@
 var giphy = $.get("https://api.giphy.com/v1/gifs/search?q=drake&api_key=dc6zaTOxFJmzC&limit=70");
 giphy.done(function(data) { 
-	var rando = Math.floor((Math.random() * 49) + 1);
-	$(document).ready(function(){
-	  	var drizzy = data.data[rando].images.original.url;
-		$('img').each(function(index, image){
-			$(image).attr('src', drizzy);
+	var imgCount = document.images.length;
+	console.log("Number of images: " + imgCount);
+	for (var i = 0; i <= imgCount ; i++) {	
+		var rando = Math.floor((Math.random() * 49) + 1);
+		$(document).ready(function(){
+		  	var drizzy = data.data[rando].images.original.url;
+			document.images[i].src = drizzy;
 		});
-	});
+	};
 	$(document).ready(function(){
 		$(document).snowfall({image :"http://www.lilwaynehq.com/forums/images/smilies/drizzy-face.png", minSize: 15, maxSize:40, flakeCount : 100, maxSpeed : 10});
 	});
